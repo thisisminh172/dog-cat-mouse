e182ea6117ca20eb821da3bf88c57b0463de2bc5
thisisminh172 <thisisminh172@gmail.com>
2020-05-25 18:07:27 +0700

    fix

--- Dog.js
+++ Dog.js
@@ -3,7 +3,7 @@ function Dog(name){
 	this.name = name;
 	this.stomach = [];
 }
-Dog.prototype.eat = funciton(cat){
+Dog.prototype.eat = function(cat){
 	this.stomach.push(cat);
 }
 Dog.prototype.sayHi = function() {
