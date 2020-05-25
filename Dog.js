1a81cb66984204c68738aaf2d4ce0ba32d3b4154
thisisminh172 <thisisminh172@gmail.com>
2020-05-25 18:02:06 +0700

    add chalk module

--- Dog.js
+++ Dog.js
@@ -1,6 +1,13 @@
-function Dog(){
+var chalk  require('chalk');
+function Dog(name){
+	this.name = name;
 	this.stomach = [];
 }
 Dog.prototype.eat = funciton(cat){
 	this.stomach.push(cat);
 }
+Dog.prototype.sayHi = function() {
+	// body...
+	console.log('Hi! i am a dog. Name is '+ chalk.red(this.name));
+};
+module.exports = Dog;
